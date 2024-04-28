9173fc6be5393506d747a9556b80845e9ff64606
namu <methichai3961@gmail.com>
2024-04-28 12:53:20 +0700

    goat command 2

--- cogs/goat.py
+++ cogs/goat.py
@@ -7,12 +7,7 @@ from discord.ext import tasks
 class goat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def sync(self, ctx) -> None:
-        fmt = await ctx.client.tree.sync(guild=ctx.guild)
-        await ctx.send(f"Synced {len(fmt)} commands.")
-
+    
     @app_commands.command(name="slash", description="test slash command")
     async def ping(self, interaction: discord.Interaction):
         await interaction.response.send_message("i kissed your mom last night!!!")

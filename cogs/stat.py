c4beb0507da5a78dd4918a3a8a46f315bb6bfa5f
namu <methichai3961@gmail.com>
2024-04-28 22:57:55 +0700

    fix

--- cogs/stat.py
+++ cogs/stat.py
@@ -11,7 +11,7 @@ class stat(commands.Cog):
     @app_commands.command(name="stat", description="gets the deltafall server stat")
     async def stat(self, interaction: discord.Interaction):
         quotes = json.load(open("data/quote.json"))
-        embed=discord.Embed(title="Statistics", description=f'{len(os.listdir("data/just_joined/"))} people came in from 2 days ago.\nThere are {len(quotes)} quote(s) being added so far', color=0x57e389)
+        embed=discord.Embed(title="Statistics", description=f'{len(os.listdir("data/just_joined/"))} people came in from 2 days ago\nThere are {len(quotes)} quote(s) added so far', color=0x57e389)
         await interaction.response.send_message(embed=embed, allowed_mentions=discord.AllowedMentions.none())
 
 async def setup(bot):

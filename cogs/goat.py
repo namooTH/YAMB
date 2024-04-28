57c65b321b9be3800a5259d4cd80d3136feb00a8
namu <methichai3961@gmail.com>
2024-04-28 13:14:11 +0700

    goat command 4

--- cogs/goat.py
+++ cogs/goat.py
@@ -1,16 +1,27 @@
 import discord
-from discord.utils import get
+import praw
 from discord.ext import commands
 from discord import app_commands
-from discord.ext import tasks
 
 class goat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reddit = praw.Reddit(client_id='-hhVNd85v5NMlKogr4Xq4g',
+        client_secret='9e-F6hhquP-5imeCBsSBfRszCodYHw',
+        user_agent='discordbot')
+        
     
     @app_commands.command(name="slash", description="test slash command")
     async def ping(self, interaction: discord.Interaction):
+
         await interaction.response.send_message("i kissed your mom last night!!!")
+
+    @app_commands.command(name="goat", description="gets random goat")
+    async def goat(self, interaction: discord.Interaction):
+        memes_submissions = self.reddit.subreddit('memes').hot()
+        print(memes_submissions)
+        #await interaction.response.send_message("i kissed your mom last night!!!")
+
 
 async def setup(bot):
     await bot.add_cog(goat(bot))

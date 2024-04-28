093d330456533e83742148ceaf376d4539418bab
namu <methichai3961@gmail.com>
2024-04-28 13:36:07 +0700

    goat command done

--- cogs/goat.py
+++ cogs/goat.py
@@ -11,12 +11,6 @@ class goat(commands.Cog):
         client_secret='9e-F6hhquP-5imeCBsSBfRszCodYHw',
         user_agent='discordbot')
         
-    
-    @app_commands.command(name="slash", description="test slash command")
-    async def ping(self, interaction: discord.Interaction):
-
-        await interaction.response.send_message("i kissed your mom last night!!!")
-
     @app_commands.command(name="goat", description="gets random goat")
     async def goat(self, interaction: discord.Interaction):
         sub = self.reddit.subreddit('goats').new()
@@ -24,7 +18,6 @@ class goat(commands.Cog):
         for i in range(0, post_to_pick):
             submission = next(x for x in sub if not x.stickied)
         await interaction.response.send_message(submission.url)
-        #await interaction.response.send_message("i kissed your mom last night!!!")
 
 
 async def setup(bot):

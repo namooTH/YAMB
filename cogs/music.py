55abcdd48b2420b4b374c30d666effd19b249022
namu <methichai3961@gmail.com>
2024-05-06 17:50:26 +0700

    format

--- cogs/music.py
+++ cogs/music.py
@@ -125,7 +125,7 @@ class music(commands.Cog):
                 progressbar += "-"
             else:
                 progressbar += "="
-        embed = Embed(title="Currently playing", description=f'# {vc.current.title}\n### by {vc.current.author}\n{progressbar}\n- {strftime("%H:%M:%S", gmtime(vc.position / 1000))} - {strftime("%H:%M:%S", gmtime(vc.current.length / 1000))}')
+        embed = Embed(title="Currently playing", description=f'# {vc.current.title}\n### by {vc.current.author}\n\n{progressbar}\n- {strftime("%H:%M:%S", gmtime(vc.position / 1000))} - {strftime("%H:%M:%S", gmtime(vc.current.length / 1000))}')
         if queue:
             embed.add_field(name="Next up:", value=f"{queue[0].title} - {queue[0].author}" , inline=True)
         embed.set_thumbnail(url=vc.current.artwork)

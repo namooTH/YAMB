ddd6d9cb2131fc8c1e6950804b13ec36001cf1e1
namu <methichai3961@gmail.com>
2024-05-08 17:47:37 +0700

    test

---
+++ cogs/graph.py
@@ -0,0 +1,27 @@
+import wavelink
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord import Embed
+from wavelink import Queue
+
+from time import strftime
+from time import gmtime
+
+from io import BytesIO
+import requests
+from PIL import Image, ImageStat
+
+class graph(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def history(ctx, cog):
+        messages = await ctx.channel.history(limit=10).flatten() # Get the history of the channel where the command was invoked
+        for message in messages: # Get only the messages and not any extra information
+            print(message.content, sep="\n") # Print the messages and show each in a new line
+
+
+async def setup(bot):
+    await bot.add_cog(graph(bot))

a7d771f01730a113b7bdc976e7bb65371c760e1d
namu <methichai3961@gmail.com>
2024-05-08 18:27:36 +0700

    piper

--- cogs/graph.py
+++ cogs/graph.py
@@ -9,6 +9,7 @@ from wavelink import Queue
 #from time import gmtime
 import datetime
 import re
+import json
 
 #from io import BytesIO
 #import requests
@@ -17,16 +18,36 @@ import re
 class graph(commands.Cog):
     def __init__(self, bot):
         self.lastmessage_time = None
+        self.allemoji = {}
         self.bot = bot
 
     @commands.command()
     async def history(self, ctx):
+        self.allemoji["lastthingbroplswhatthefuck"] = self.lastmessage_time
+        json.dump(self.allemoji, open("data/emoji.json", 'w'), indent=2)
+        #entireting = r'(<.*?>)'
+        justtheemojiname = r'<:(.*?):\d+>'
         if ctx.author.id == 899113384660844634:
+            if not self.lastmessage_time:
+                async for message in ctx.channel.history(limit=500, oldest_first=True):
+                    finds = re.findall(justtheemojiname, message.content)
+                    for emoji in finds:
+                        if emoji in self.allemoji:
+                            self.allemoji[emoji] += 1
+                        else:
+                            self.allemoji[emoji] = 1
+                    self.lastmessage_time = message.created_at
+                return await self.history(self, ctx)
+
             async for message in ctx.channel.history(limit=500, after=self.lastmessage_time):
-                finds = re.findall(r'<:(.*?):\d+>', message.content)
+                finds = re.findall(justtheemojiname, message.content)
                 for emoji in finds:
-                    print(finds)
+                    if emoji in self.allemoji:
+                        self.allemoji[emoji] += 1
+                    else:
+                        self.allemoji[emoji] = 1
                 self.lastmessage_time = message.created_at
+            return await self.history(self, ctx)
 
 
 

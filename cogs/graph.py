e3fa2f777de1fbcd5f720ca266c9baa033b783a3
namu <methichai3961@gmail.com>
2024-05-08 18:09:04 +0700

    sex

--- cogs/graph.py
+++ cogs/graph.py
@@ -1,25 +1,33 @@
-import wavelink
+#import wavelink
 import discord
 from discord.ext import commands
 from discord import app_commands
 from discord import Embed
 from wavelink import Queue
 
-from time import strftime
-from time import gmtime
+#from time import strftime
+#from time import gmtime
+import datetime
+import re
 
-from io import BytesIO
-import requests
-from PIL import Image, ImageStat
+#from io import BytesIO
+#import requests
+#from PIL import Image, ImageStat
 
 class graph(commands.Cog):
     def __init__(self, bot):
+        self.lastmessage_time = None
         self.bot = bot
 
     @commands.command()
     async def history(self, ctx):
-        async for message in ctx.channel.history(limit=500):
-            print(message.content) # Print the messages and show each in a new line
+        if ctx.author.id == 899113384660844634:
+            async for message in ctx.channel.history(limit=500, after=self.lastmessage_time):
+                finds = re.findall(r'<:(.*?):\d+>', message.content)
+                for emoji in finds:
+                    print(finds)
+                self.lastmessage_time = message.created_at
+
 
 
 async def setup(bot):

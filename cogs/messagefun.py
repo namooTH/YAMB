c9dfba65190bc11aeb2117d8c29f1ab84bd81b09
namu <methichai3961@gmail.com>
2024-05-12 17:07:35 +0700

    amazing

--- cogs/messagefun.py
+++ cogs/messagefun.py
@@ -14,10 +14,9 @@ class messagefun(commands.Cog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
-        match message.content.lower():
-            case "@someone":
-                members = message.guild.members
-                await message.channel.send(f'<@{members[random.randint(0, len(members) - 1)].id}>', reference=message)
+        if message.content.lower() in "@someone":
+            members = message.guild.members
+            await message.channel.send(f'<@{members[random.randint(0, len(members) - 1)].id}>', reference=message)
 
 async def setup(bot):
     await bot.add_cog(messagefun(bot))

58333702f6b4ca4d25f9715bb2793280eaa0c457
namu <methichai3961@gmail.com>
2024-05-12 19:05:36 +0700

    stupid python :

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -16,7 +16,7 @@ class mod(commands.Cog):
             return
 
         message_content = message.content.lower()
-        if not message_content.startswith(".m ") and not message.author.guild_permissions.manage_messages:
+        if not message_content.startswith(".m ") or not message.author.guild_permissions.manage_messages:
             return
         action = message_content[3:]
         if "," in action:

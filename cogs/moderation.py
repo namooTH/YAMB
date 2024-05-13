23b4178f0dd01f95198f5d7813bf90a46bf16b05
namu <methichai3961@gmail.com>
2024-05-13 12:56:00 +0700

    removed unused lib

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,10 +2,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-#import random
-#from typing import Optional
-import yaml
-
 class mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot

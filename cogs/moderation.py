cb0e422283db26999dffa19f32a72d4e73b4bfc9
Namu <73419369+namooTH@users.noreply.github.com>
2024-05-26 19:00:15 +0700

    adds timeout

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,6 +12,20 @@ class mod(commands.Cog):
     async def delete(self, message):
         await message.delete()
 
+    async def timeout(self, user, rawlength, reason=None):
+        unit_map = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
+        number = ""
+        length_type = ""
+        for st in rawlength:
+            if st.isalpha():
+                length_type += st
+            else:
+                number += st
+        number = int(number)    
+        unit = unit_map.get(length_type)
+        if unit == None: unit = length_type
+        await user.timeout(until=(timedelta(**{unit: number})), reason=reason)
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
@@ -44,6 +58,14 @@ class mod(commands.Cog):
                             else:
                                 errors.append(f"invaild action at `{var[0]}`: {type(var[1])} not in {allowedtype}")
                                 break
+
+                        case "t" | "timeout":
+                            messager = await message.channel.fetch_message(message.reference.message_id)
+                            allowedtype = (str)
+                            if isinstance(var[1], allowedtype):
+                                await self.timeout(user=messager.author, rawlength=var[1])
+                            else:
+                                errors.append(f"invaild action at `{var[0]}`: {type(var[1])} not in {allowedtype}")
 
                         # what the fuck
                         case _:

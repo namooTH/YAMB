7d706f2860430e6a7d6d8acb4d333f549b078f81
Namu <73419369+namooTH@users.noreply.github.com>
2024-06-14 20:14:29 +0700

    hold

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -12,8 +12,8 @@ class reaction(commands.Cog):
         channel = reaction.message.channel
         if channel.id == 1198291214672347311:
             print()
-            if reaction.emoji == "👍":
-                await channel.send(f'test')
+            if reaction.emoji == "❤️":
+                await channel.send('https://media.discordapp.net/attachments/1198291214672347311/1251094400684261467/20240614_062850.jpg?ex=666d53f5&is=666c0275&hm=2f16d4ca2ad8c69f464d561f07c6710b8c01280a8ea7d8a887dd5f90052a65c1&=&format=webp&width=1198&height=590')
 
 async def setup(bot):
     await bot.add_cog(reaction(bot))

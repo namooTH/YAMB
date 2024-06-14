4d192f59ab4feb2ad3d70f4909585bd76e8a73ef
Namu <73419369+namooTH@users.noreply.github.com>
2024-06-14 20:11:48 +0700

    tst

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -11,7 +11,9 @@ class reaction(commands.Cog):
     async def on_reaction_add(self, reaction, user):
         channel = reaction.message.channel
         if channel.id == 1198291214672347311:
-            await channel.send('{} has added {} to the the message {}'.format(user.name, reaction.emoji, reaction.message.content))
+            print()
+            if reaction.emoji == "👍":
+                await channel.send(f'test')
 
 async def setup(bot):
     await bot.add_cog(reaction(bot))

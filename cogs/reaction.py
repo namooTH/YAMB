b0440d2f9a0094d389878f383c855e84ee3d682f
Namu <73419369+namooTH@users.noreply.github.com>
2024-06-14 20:23:22 +0700

    yes

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord import app_commands
 from typing import Optional
 
+import asyncio
+
 class reaction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,7 +14,10 @@ class reaction(commands.Cog):
         channel = reaction.message.channel
         if channel.id == 1198291214672347311:
             if reaction.emoji == "❤️" and reaction.count <= 1:
-                await reaction.message.reply('https://media.discordapp.net/attachments/1198291214672347311/1251094400684261467/20240614_062850.jpg?ex=666d53f5&is=666c0275&hm=2f16d4ca2ad8c69f464d561f07c6710b8c01280a8ea7d8a887dd5f90052a65c1&=&format=webp&width=1198&height=590')
+                reply_msg = await reaction.message.reply('https://media.discordapp.net/attachments/1198291214672347311/1251094400684261467/20240614_062850.jpg?ex=666d53f5&is=666c0275&hm=2f16d4ca2ad8c69f464d561f07c6710b8c01280a8ea7d8a887dd5f90052a65c1&=&format=webp&width=1198&height=590')
+                await asyncio.sleep(5)
+                await reply_msg.delete()
+
 
 async def setup(bot):
     await bot.add_cog(reaction(bot))
